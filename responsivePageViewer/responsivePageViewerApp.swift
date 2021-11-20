e9c0e45a33a4d22304bb64a797d68709d8c715c4
MaxKulakov <kulakovmaxim99@gmail.com>
2021-11-20 18:18:33 +0400

    Initial Commit

---
+++ responsivePageViewer/responsivePageViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  responsivePageViewerApp.swift
+//  responsivePageViewer
+//
+//  Created by Max Kulakov on 20.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct responsivePageViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
